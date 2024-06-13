7f1112da70f98ec62054ce33739529f4dc1550a6
Hugo Guedes <hugoguedes@ipvc.pt>
2024-06-14 00:24:00 +0100

    no message

--- database/seeders/Dev/DevUserSeeder.php
+++ database/seeders/Dev/DevUserSeeder.php
@@ -15,8 +15,8 @@ class DevUserSeeder extends Seeder
     {
         $data = [
             ['name' => 'Hugo Guedes', 'email' => 'hugo.guedes@riftweb.com', 'username' => 'Jhon', 'password' => '123456789', 'phone_number' => '999999999', 'profile_picture' => 'test', 'description' => 'hhhhh'],
-            ['name' => 'TestName 1', 'email' => 'hugo.guedes@riftweb.com', 'username' => 'Jhon2', 'password' => '123456789', 'phone_number' => '999999999', 'profile_picture' => 'test', 'description' => 'hhhhh'],
-            ['name' => 'TestName 2', 'email' => 'hugo.guedes@riftweb.com', 'username' => 'Jhon3', 'password' => '123456789', 'phone_number' => '999999999', 'profile_picture' => 'test', 'description' => 'hhhhh']
+            ['name' => 'TestName 1', 'email' => 'hugo.guede@riftweb.com', 'username' => 'Jhon2', 'password' => '123456789', 'phone_number' => '999999999', 'profile_picture' => 'test', 'description' => 'hhhhh'],
+            ['name' => 'TestName 2', 'email' => 'hugo.gued@riftweb.com', 'username' => 'Jhon3', 'password' => '123456789', 'phone_number' => '999999999', 'profile_picture' => 'test', 'description' => 'hhhhh'],
         ];
 
         foreach ($data as $item) {
